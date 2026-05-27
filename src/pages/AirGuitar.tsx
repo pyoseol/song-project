78c70843b452ad9bef9e3a9a68c91691abc969ce
BerreFermata <simjun6245@gmail.com>
2026-05-27 15:25:42 +0900

    에어 기타 프로토타입 제작

---
+++ src/pages/AirGuitar.tsx
@@ -0,0 +1,10 @@
+import HandTracker from '../components/HandTracker'
+
+export default function AirGuitar() {
+  return (
+    <div>
+      <h1>Air Guitar Test</h1>
+      <HandTracker />
+    </div>
+  )
+}
